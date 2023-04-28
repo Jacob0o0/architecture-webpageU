43b81bc77f424c20da3eebd35878d83523850125
Jacobo <jacoboe127@gmail.com>
2023-04-27 18:43:30 -0600

    Getting ready with php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Espacio Urbano</title>
+</head>
+<body>
+    <div class="nav-bar">Barra navegación
+        <a href="index.php">Inicio</a>
+        <a href="edificios.php">Edificios</a>
+    </div>
+
+    <div class="main-image">IMAGEN PRINCIPAL</div>
+
+    <div class="container">
+        <div class="row container-section">
+            <div class="card col-4">jijii</div>
+            <div class="card col-4">jojo</div>
+            <div class="card col-4">jojo</div>
+            <div class="card col-4">jijii</div>
+            <div class="card col-4">jojo</div>
+            <div class="card col-4">jojo</div>
+        </div>
+    </div>
+
+
+    <div class="container-section col-12">container de espacios urbanos</div>
+
+    <div class="container-section col-12">container de edificios</div>
+
+    <div class="container-section col-12">container de biografias</div>
+
+    <div class="footer footer-J">
+        Pie de página
+    </div>
+</body>
+</html>
